50b16413d07c30849f2514fe3baddac3d56e7540
LuketoDev <lucasbravouni@gmail.com>
2025-07-02 05:03:23 -0300

    variable de entorno api puesta con la api original que funcionaba

--- src/main/java/com/tallerwebi/dominio/ServicioDolar.java
+++ src/main/java/com/tallerwebi/dominio/ServicioDolar.java
@@ -12,7 +12,7 @@ public class ServicioDolar {
     private final String API_URL = (System.getenv("API_DOLAR") != null)
                                     ? System.getenv("API_DOLAR")
                                     : "https://dolarapi.com/v1/dolares/blue";
-
+    // en ves de pegarle a este muchas veces deberia guardarlo una vez inicia la aplicacion y luego hacer el calculo, pero la variable de entorno funciona
     public Double obtenerCotizacionDolarBlue() {
         Map<String, Object> response = restTemplate.getForObject(API_URL, Map.class); //Hace la solicitud HTTP GET a la URL
         if (response != null && response.containsKey("venta")) {
